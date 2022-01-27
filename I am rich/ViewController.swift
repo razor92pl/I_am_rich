d385c157c240e68f72f334fef080444f247020e3
janczak.patryk <janczak.patryk92@gmail.com>
2022-01-27 23:01:58 +0100

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Patryk Jańczak on 27/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
